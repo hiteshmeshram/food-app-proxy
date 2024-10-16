a0075995a5ee1705c47edf9c830a087c218d58db
hiteshmeshram <hiteshmeshramt@gmail.com>
2024-10-16 23:28:05 +0530

    checkpoint1

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express();
+const PORT = 3000;
+
+app.get('/data',(req,res)=>{
+    res.json("data ")
+})
+
+app.listen(PORT,()=>console.log(`server running on port${PORT}`))
