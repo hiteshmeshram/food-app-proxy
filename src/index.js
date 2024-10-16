9e8ffd55908103f47844bae02a7cfdf1dbd77b46
hiteshmeshram <hiteshmeshramt@gmail.com>
2024-10-17 00:02:51 +0530

    check3

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.get('/data',(req,res)=>{
     res.json("data ")
 })
 
-app.get('restaurant',async (req,res)=>{
+app.get('/restaurant',async (req,res)=>{
     const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
     const json=await data.json();
 
